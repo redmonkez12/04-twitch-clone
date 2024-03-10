5bdc54111f1d9d5922bcd892aedfba0d9f26d102
Tomas Svojanovsky <tomas.svojanovsky33@gmail.com>
2024-03-10 20:21:49 +0100

    Dark mode

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,8 +1,10 @@
 import { PropsWithChildren } from "react";
+import { Logo } from "@/app/(auth)/_components/logo";
 
 export default function AuthLayout({ children }: PropsWithChildren) {
     return (
-        <div className="h-full flex items-center justify-center">
+        <div className="h-full flex flex-col items-center justify-center space-y-6">
+            <Logo/>
             {children}
         </div>
     );
